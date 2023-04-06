d35cbd5a88e41c58ef47fce01eafcb5b32210c10
Iyanu456 <oyerindei13@gmail.com>
2023-04-06 01:19:57 +0100

    default message

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# bash script that sets up your web servers for the deployment of web_static
+
+folders=("models" "data" "data/web_static" "/data/web_static/releases" "/data/web_static/shared" "/data/web_static/releases/test")
+
+for folder in "${folders[@]}"
+do
+	if [ ! -d "$folder" ]; then
+		mkdir "$folder"
+	fi
+done
+echo "<p>Hello World!</p>" > ./data/web_static/releases/test/index.html
+
+# Remove existing symbolic link if it exists
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+fi
+
+ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+server {
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+}
+sudo service nginx restart
+
