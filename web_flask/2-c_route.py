baf37ac8c9d7816566437acb07f9bf4d34a08e23
OnyeEbube <okontaonyeka@gmail.com>
2023-04-20 13:23:17 +0000

    Adding 3-python_route.py file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB" with root '/'
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" with root '/hbnb'
+    """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cTxt(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space
+    """
+    return ("C {}".format(text.replace("_", " ")))
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
